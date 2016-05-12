fc9091463bad4b45be69ff411e50a0e657b3407e
Ivan Cheung <studiozerotoronto@gmail.com>
2016-05-12 19:07:11 -0400

    Added view controllers

--- Demo/Demo/FirstViewController.swift
+++ Demo/Demo/FirstViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FirstViewController: UIViewController {
 
+    @IBOutlet weak var uiLabelName: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.uiLabelName.text = "Hi Ivan";
     }
 
     override func didReceiveMemoryWarning() {
